121c57dfcb17a0ee09e6081bbafd0bb06d803312
khinydnlin <145341635+khinydnlin@users.noreply.github.com>
2024-04-22 21:51:31 +0100

    changed config setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+  site: 'https://khinydnlin.github.io',
+  base: '/',
   integrations: [mdx(), sitemap(), tailwind()]
 });
