fe3237fda8f11119947dbd6f2c3b61dac9d55db4
Khin Yadanar Lin <145341635+khinydnlin@users.noreply.github.com>
2024-04-23 11:38:44 +0100

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astrofy | Personal Portfolio Website Template';
-export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_TITLE = 'Khin Yadanar Lin | Portfolio Website';
+export const SITE_DESCRIPTION = 'Data Science and Analytics Professional | London ';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
